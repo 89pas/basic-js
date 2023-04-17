199e3c09e697462c142da58605570e98021d080f
89pas <112816238+89pas@users.noreply.github.com>
2023-04-18 01:33:53 +0300

    feat: task

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,28 +1,31 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
 /**
- * Given a number, replace this number with
- * the sum of its digits until we get to a one digit number.
+ * Given an array with heights, sort them except if the value is -1.
  *
- * @param {Number} n
- * @return {Number}
+ * @param {Array} arr
+ * @return {Array}
  *
  * @example
- * For 100, the result should be 1 (1 + 0 + 0 = 1)
- * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
+ * arr = [-1, 150, 190, 170, -1, -1, 160, 180]
  *
+ * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function getSumOfDigits(n) {
-  let arr = String(n)
-    .split("")
-    .map((el) => +el);
-  let result = arr.reduce((acc, curr) => acc + curr, 0);
-  if (result >= 10) {
-    return getSumOfDigits(result);
+function sortByHeight(arr) {
+  let sorted = arr.filter((el) => el !== -1).sort((a, b) => a - b);
+  let result = [];
+  let j = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === -1) {
+      result.push(-1);
+    } else {
+      result.push(sorted[j]);
+      j++;
+    }
   }
   return result;
 }
 
 module.exports = {
-  getSumOfDigits
+  sortByHeight
 };

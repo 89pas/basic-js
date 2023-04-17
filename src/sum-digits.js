a08344c18d2710ecccb80ff933757b019a337c3f
89pas <112816238+89pas@users.noreply.github.com>
2023-04-18 01:31:07 +0300

    feat: task

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -1,22 +1,52 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
 /**
- * Given a number, replace this number with
- * the sum of its digits until we get to a one digit number.
+ * Create transformed array based on the control sequences that original
+ * array contains
  *
- * @param {Number} n
- * @return {Number}
+ * @param {Array} arr initial array
+ * @returns {Array} transformed array
  *
  * @example
- * For 100, the result should be 1 (1 + 0 + 0 = 1)
- * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
+ *
+ * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
+ * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+	if (!Array.isArray(arr)) {
+		throw new Error (`'arr' parameter must be an instance of the Array!`);
+	}
+	let newArr = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] === '--discard-next') {
+			i++;
+		} else if (arr[i] === '--discard-prev') {
+			if (arr[i - 2] === '--discard-next') {
+				continue;
+			}
+			if (arr[i - 1]) {
+				newArr.pop();
+			}
+		} else if (arr[i] === '--double-next') {
+			if (arr[i + 1]) {
+				newArr.push(arr[i + 1]);
+			}
+		} else if (arr[i] === '--double-prev') {
+			if (arr[i - 2] === '--discard-next') {
+				continue;
+			}
+			if (arr[i - 1]) {
+				newArr.push(arr[i - 1]);
+			}
+		} else {
+			newArr.push(arr[i]);
+		}
+	}
+
+	return newArr;
 }
 
 module.exports = {
-  getSumOfDigits
+  transform
 };
